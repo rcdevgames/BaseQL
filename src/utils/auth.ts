4928c9c9989a16c0569b8fc4526fc68ba830027b
rmodz <rcdev.games@gmail.com>
2021-04-01 01:18:41 +0700

    check lib, auth, builder, update package, and add global helper

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,15 +3,28 @@ import { Context } from 'graphql-yoga/dist/types';
 import { decryptToken } from './jwt';
 
 const authenticate = (context: Context) => {
-  const Authorization = context.request.get('Authorization');
-
-  if (Authorization) {
-    const token = Authorization.replace('Bearer ', '');
-    const userId = decryptToken<String>(token);
-    return userId;
-  }
-
-  throw new Error('Not authorized');
+  try {
+    const Authorization = context.request.get('Authorization');
+  
+    if (Authorization) {
+      const token = Authorization.replace('Bearer ', '');
+      const userData = decryptToken<{}>(token);
+  
+      // Cleansing Default Data from JWT
+      delete userData['iat'];
+      delete userData['exp'];
+      delete userData['aud'];
+      delete userData['iss'];
+  
+      return userData;
+    }
+  } catch (_) {}
+  return null;
 };
 
-export { authenticate };
+const isAuth = (ctx: Context) => {
+  if (ctx.user === null) throw "Unauthorized";
+  return ctx.user;
+}
+
+export { authenticate, isAuth };
--- src/utils/builder.ts
+++ src/utils/builder.ts
@@ -1,4 +1,5 @@
 import { pool } from './db';
+import { converDatetoISO } from './global';
 
 const convertFields = (fields: any = []) => {
     let callback = (fields.length > 0 ? '' : '');
@@ -11,10 +12,14 @@ const convertFields = (fields: any = []) => {
 const convertConditions = (conditions: any = {}) => {
     let callback = (Object.keys(conditions).length > 0 ? ' WHERE ' : '');
     Object.keys(conditions).forEach((key, index) => {
-        if (typeof conditions[key] === 'string') {
-        conditions[key] = `'${conditions[key]}'`;
+        if (conditions[key] == null) {
+          callback += key;
+        }else {
+          if (typeof conditions[key] === 'string') {
+            conditions[key] = `'${conditions[key]}'`;
+          }
+          callback += `${key} = ${conditions[key]}`;
         }
-        callback += `${key} = ${conditions[key]}`;
         callback += (index < (Object.keys(conditions).length - 1) ? ' AND ' : '');
     })
     return callback;
@@ -38,13 +43,28 @@ const convertOrders = (orders: any = []) => {
     })
     return callback;
 }
+const converDateISO = (objectData: object = {}) => {
+  if (Object.keys(objectData).length > 0) {
+    let res = {};
+    Object.keys(objectData).forEach((v) => {
+      if (objectData[v] instanceof Date) {
+        res[v] = converDatetoISO(objectData[v]);
+      }else {
+        res[v] = objectData[v];
+      }
+    });
+    return res;
+  }else {
+    return objectData;
+  }
+}
 
 const get = async (
     tableName: string, 
     fields: any, 
     conditions: any = {},
     orders: any = {},
-    joins: any = [],
+    joins: Array<{type?: string, target: string, condition: string}> = [],
     limit: number = 0,
     offset: number = 0
 ) => {
@@ -66,39 +86,39 @@ const get = async (
           ${limitCondition} ${offsetCondition}
         `;
         // RETURNING
+        console.log(query);
         const { rows } = await conn.query(query);
         conn.release();
-        return rows;
+        return rows.map((v) => converDateISO(v));
     } catch (err) {
         throw err.message +'\n sytanx: '+ query;
     }
 }
 const getOne = async (
     tableName: string,
-    fields: any,
-    conditions: any = {},
-    orders: any = {},
-    joins: any = [],
-    limit: number = 0,
-    offset: number = 0
+    fields: string[],
+    conditions: Object = {},
+    joins: Array<{type?: string, target: string, condition: string}> = []
   ) => {
     try {
-      const data = await get(tableName,fields,conditions,orders,joins,limit,offset);
+      const data = await get(tableName,fields,conditions,{},joins);
       if (data.length > 0) {
-        return data[0];
+        return converDateISO(data[0]);
       } else {
-        return {};
+        return null;
       }
     } catch (err) {
       throw err;
     }
   }
-const getCount = async (tableName: string, conditions: any = {}) => {
+const getCount = async (tableName: string, conditions: any = {}, joins: Array<{type?: string, target: string, condition: string}> = []) => {
     try {
       const conn = await pool.connect();
       let whereConditions = convertConditions(conditions);
+      let joinConditions: string = convertJoins(joins);
       const { rows } = await pool.query(`
         SELECT COUNT(*) AS count FROM ${tableName} 
+        ${joinConditions}
         ${whereConditions}
       `);
       conn.release();
@@ -108,65 +128,94 @@ const getCount = async (tableName: string, conditions: any = {}) => {
     }
   }
 const insert = async (tableName: string, data: any = {}) => {
-    let query:string = '';
-    try {
-      const conn = await pool.connect();
+  let query:string = '';
+  try {
+    const conn = await pool.connect();
+    let fields = '';
+    let values = '';
+    Object.keys(data).forEach((key: any, i: any) => {
+      const comma = (i < (Object.keys(data).length - 1) ? ',' : '');
+      fields += key;
+      fields += comma;
+      values += `'${data[key]}'`;
+      values += comma;
+    })
+    query = `
+      INSERT INTO ${tableName} 
+      (${fields}) VALUES (${values})
+    `;
+    // RETURNING
+    const { rows } = await conn.query(query);
+    conn.release();
+    return rows;
+  } catch (err) {
+    throw err.message + '\n sytanx: ' + query;
+  }
+}
+const multiInsert = async (tableName: string, data: Array<{}>) => {
+  let query:string = '';
+  const conn = await pool.connect();
+  try {
+    data.forEach((val) => {
       let fields = '';
       let values = '';
-      Object.keys(data).forEach((key: any, i: any) => {
-        const comma = (i < (Object.keys(data).length - 1) ? ',' : '');
+      Object.keys(val).forEach((key: any, i: any) => {
+        const comma = (i < (Object.keys(val).length - 1) ? ',' : '');
         fields += key;
         fields += comma;
-        values += `'${data[key]}'`;
+        values += `'${val[key]}'`;
         values += comma;
       })
-      query = `
-        INSERT INTO ${tableName} 
-        (${fields}) VALUES (${values})
-      `;
-      // RETURNING
-      const { rows } = await conn.query(query);
-      conn.release();
-      return rows;
-    } catch (err) {
-      throw err.message + '\n sytanx: ' + query;
-    }
+      query += `INSERT INTO ${tableName} (${fields}) VALUES (${values}); \n`;
+    });
+    
+    await conn.query('BEGIN');
+    await conn.query(query);
+    await conn.query('COMMIT');
+    // RETURNING
+    conn.release();
+    return true;
+  } catch (err) {
+    await conn.query('ROLLBACK');
+    throw err.message + '\n sytanx: ' + query;
   }
-const update = async (tableName: string, data: any, conditions: any) => {
-    let query: string = '';
-    try {
-      const conn = await pool.connect();
-      let fields = '';
-      let whereConditions = convertConditions(conditions);
-      Object.keys(data).forEach((key: any, i: any) => {
-        let value = data[key];
-        if (typeof value === 'string') {
-          value = `'${value}'`;
-        }
-        fields += ` ${key} = ${value} `;
-        fields += (i < (Object.keys(data).length - 1) ? ',' : '');
-      })
-      query = `
-        UPDATE ${tableName} SET ${fields} 
-        ${whereConditions} 
-      `;
-      // RETURNING
-      const { rows } = await conn.query(query);
-      conn.release();
-      return rows;
-    } catch (err) {
-      throw err.message + '\n sytanx: ' + query;
-    }
+
+}
+const update = async (tableName: string, data: Object, conditions: {} = {}) => {
+  let query: string = '';
+  try {
+    const conn = await pool.connect();
+    let fields = '';
+    let whereConditions = convertConditions(conditions);
+    Object.keys(data).forEach((key: any, i: any) => {
+      let value = data[key];
+      if (typeof value === 'string') {
+        value = `'${value}'`;
+      }
+      fields += ` ${key} = ${value} `;
+      fields += (i < (Object.keys(data).length - 1) ? ',' : '');
+    })
+    query = `
+      UPDATE ${tableName} SET ${fields} 
+      ${whereConditions} 
+    `;
+    // RETURNING
+    const { rows } = await conn.query(query);
+    conn.release();
+    return rows;
+  } catch (err) {
+    throw err.message + '\n sytanx: ' + query;
+  }
 }
 const manual = async (query: string) => {
-    try {
-      const conn = await pool.connect();
-      const { rows } = await conn.query(query);
-      conn.release();
-      return rows;
-    } catch (err) {
-      throw err.message + '\n sytanx: ' + query;
-    }
+  try {
+    const conn = await pool.connect();
+    const { rows } = await conn.query(query);
+    conn.release();
+    return rows;
+  } catch (err) {
+    throw err.message + '\n sytanx: ' + query;
+  }
 }
 
 export {
@@ -174,6 +223,7 @@ export {
     getOne,
     getCount,
     insert,
+    multiInsert,
     update,
     manual
 }
